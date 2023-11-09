e1be7f9bdaa59e5d6da4f0501af941a4778d911c
Murtaza2921 <88159473+Murtaza2921@users.noreply.github.com>
2023-11-09 10:43:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cheerio = require("cheerio");
 const app=express().use(body_parser.json());
 
 const token=process.env.TOKEN;
-const mytoken=process.env.MYTOKEN;//prasath_token
+const mytoken=process.env.MYTOKEN;
 
 app.listen(process.env.PORT,()=>{
     console.log("webhook is listening");
